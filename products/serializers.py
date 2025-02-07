886d34aa38dd9b6ee0092129a5fe6612bd8278b9
ana <annagorgodze8@gmail.com>
2025-02-07 21:50:37 +0400

    homework

--- products/serializers.py
+++ products/serializers.py
@@ -1,123 +1,153 @@
-from rest_framework import serializers
-from products.models import Review, Product, ProductTag, FavoriteProduct
+from rest_framework import serializers 
+from products.models import Review, Product, ProductTag, FavoriteProduct 
 
-class ProductSerializer(serializers.Serializer):
-    name = serializers.CharField()
-    description = serializers.CharField()
-    price = serializers.FloatField()
-    currency = serializers.ChoiceField(choices=['GEL', 'USD', 'EURO'])
+ 
 
-    def validate_price(self, value):
-        # produqtis fasi arunda iyos uaryofiti
+class ProductSerializer(serializers.Serializer): 
+    name = serializers.CharField() 
+    description = serializers.CharField() 
+    price = serializers.FloatField() 
+    currency = serializers.ChoiceField(choices=['GEL', 'USD', 'EURO']) 
+    def validate_price(self, value): 
+        # produqtis fasi arunda iyos uaryofiti 
+        if value < 0: 
+            raise serializers.ValidationError("Price cannot be negative.") 
 
-        if value < 0:
-            raise serializers.ValidationError("Price cannot be negative.")
-        return value
+        return value 
+
+class CartSerializer(serializers.Serializer): 
+    product_id = serializers.IntegerField() 
+
+    quantity = serializers.IntegerField(min_value=1) 
+    def validate_product_id(self, value): 
+
+        #vamowmebt aris tuara bazashi es produqti 
+
+        if not Product.objects.filter(id=value).exists(): 
+
+            raise serializers.ValidationError("Invalid product_id. Product does not exist.") 
+
+        return value 
+
+class ReviewSerializer(serializers.Serializer): 
+
+    product_id = serializers.IntegerField(write_only=True) 
+
+    content = serializers.CharField() 
+
+    rating = serializers.IntegerField() 
+    def validate_product_id(self, value): 
+        #produqtis arsebobis shemowmeba bazashi 
+
+        try: 
+
+            Product.objects.get(id=value) 
+
+        except Product.DoesNotExist: 
+
+            raise serializers.ValidationError("Invalid product_id. Product does not exist.") 
+
+        return value 
+
+    def validate_rating(self, value): 
+
+        #reitingi unda ios 1-dan 5mde,anu shefaseba igive 
+
+        if value < 1 or value > 5: 
+
+            raise serializers.ValidationError("Rating must be between 1 and 5.") 
+
+        return value 
+    def create(self, validated_data): 
+
+        product = Product.objects.get(id=validated_data['product_id']) 
+
+        user = self.context['request'].user 
+
+        #vqmnit axal mimoxilvas arsebuli momxmareblistvis da produqtistvis 
+
+        review = Review.objects.create( 
+
+            product=product, 
+
+            user=user, 
+
+            content=validated_data['content'], 
+
+            rating=validated_data['rating'], 
+
+        ) 
+
+        return review 
+
+class ProductTagSerializer(serializers.ModelSerializer): 
+
+    product_id = serializers.IntegerField(write_only=True) 
+
+    tag_name = serializers.CharField() 
+    class Meta: 
+
+        model = ProductTag 
+
+        fields = ['id', 'product_id', 'tag_name'] 
+
+    def validate_product_id(self, value): 
+
+        #produqtis arsebobis shemowmeba aucilebelia, rata ar davamatot tags ararsebul produqtze 
+
+        if not Product.objects.filter(id=value).exists(): 
+
+            raise serializers.ValidationError("Invalid product_id. Product does not exist.") 
+
+        return value 
+    def validate_tag_name(self, value): 
+
+        #tag-is saxeli ar sheidzleba iyos carieli da unda iyso maqsimum 50 simbolo 
+        if not value.strip(): 
+            raise serializers.ValidationError("Tag name cannot be empty.") 
+
+        if len(value) > 50: 
+
+            raise serializers.ValidationError("Tag name is too long. Maximum 50 characters allowed.") 
+
+        return value 
+    def create(self, validated_data): 
+
+        product = Product.objects.get(id=validated_data['product_id']) 
+
+        tag, created = ProductTag.objects.get_or_create(product=product, tag_name=validated_data['tag_name']) 
+
+        return tag 
+class FavoriteProductSerializer(serializers.ModelSerializer): 
+
+    class Meta: 
+
+        model = FavoriteProduct 
+
+        fields = ['id', 'user', 'product'] 
+
+        read_only_fields = ['user'] #momxmareblis veli mxolod wasakitxia,radgan is sistemashi avtomaturad emateba  
+
+    def validate(self, data): 
+
+        user = self.context['request'].user 
+
+        product = data.get('product') 
+
+        #vamowmebt aris tuara produqti ukve damatebuli favoritebshi aam momxmareblistvis 
+
+        if FavoriteProduct.objects.filter(user=user, product=product).exists(): 
+
+            raise serializers.ValidationError("This product is already in your favorites.") 
+
+        return data 
+    def create(self, validated_data): 
+
+        user = self.context['request'].user 
+
+        favorite_product = FavoriteProduct.objects.create(user=user, **validated_data) 
+
+        return favorite_product 
 
 
-class CartSerializer(serializers.Serializer):
-    product_id = serializers.IntegerField()
-    quantity = serializers.IntegerField(min_value=1)
-
-<<<<<<< HEAD
-    def validate_product_id(self, value):
-        #vamowmebt aris tuara bazashi es produqti
-        if not Product.objects.filter(id=value).exists():
-            raise serializers.ValidationError("Invalid product_id. Product does not exist.")
-        return value
-
-
-class ReviewSerializer(serializers.Serializer):
-    product_id = serializers.IntegerField(write_only=True)
-    content = serializers.CharField()
-    rating = serializers.IntegerField()
-
-    def validate_product_id(self, value):
-        #produqtis arsebobis shemowmeba bazashi
-        try:
-            Product.objects.get(id=value)
-        except Product.DoesNotExist:
-=======
-class ReviewSerializer(serializers.ModelSerializer):
-    product_id = serializers.IntegerField(write_only=True)
-
-    class Meta:
-        model = Review
-        fields = ['product_id', 'content', 'rating']
-
-    def validate_product_id(self, value):
-        if not Product.objects.filter(id=value).exists():
->>>>>>> 859a6da604bc5dfa33291e864af507a5aff7e930
-            raise serializers.ValidationError("Invalid product_id. Product does not exist.")
-        return value
-
-    def validate_rating(self, value):
-        #reitingi unda ios 1-dan 5mde,anu shefaseba igive
-        if value < 1 or value > 5:
-            raise serializers.ValidationError("Rating must be between 1 and 5.")
-        return value
-
-    def create(self, validated_data):
-        product = Product.objects.get(id=validated_data.pop('product_id'))
-        user = self.context['request'].user
-<<<<<<< HEAD
-        #vqmnit axal mimoxilvas arsebuli momxmareblistvis da produqtistvis
-        review = Review.objects.create(
-            product=product,
-            user=user,
-            content=validated_data['content'],
-            rating=validated_data['rating'],
-        )
-        return review
-
-
-=======
-        return Review.objects.create(product=product, user=user, **validated_data)
-    
->>>>>>> 859a6da604bc5dfa33291e864af507a5aff7e930
-class ProductTagSerializer(serializers.ModelSerializer):
-    product_id = serializers.IntegerField(write_only=True)
-    tag_name = serializers.CharField()
-
-    class Meta:
-        model = ProductTag
-        fields = ['id', 'product_id', 'tag_name']
-
-    def validate_product_id(self, value):
-        #produqtis arsebobis shemowmeba aucilebelia, rata ar davamatot tags ararsebul produqtze
-        if not Product.objects.filter(id=value).exists():
-            raise serializers.ValidationError("Invalid product_id. Product does not exist.")
-        return value
-
-    def validate_tag_name(self, value):
-        
-        #tag-is saxeli ar sheidzleba iyos carieli da unda iyso maqsimum 50 simbolo
-        if not value.strip():
-            raise serializers.ValidationError("Tag name cannot be empty.")
-        if len(value) > 50:
-            raise serializers.ValidationError("Tag name is too long. Maximum 50 characters allowed.")
-        return value
-
-    def create(self, validated_data):
-        product = Product.objects.get(id=validated_data['product_id'])
-        tag, created = ProductTag.objects.get_or_create(product=product, tag_name=validated_data['tag_name'])
-        return tag
-
-
-class FavoriteProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = FavoriteProduct
-        fields = ['id', 'user', 'product']
-        read_only_fields = ['user'] #momxmareblis veli mxolod wasakitxia,radgan is sistemashi avtomaturad emateba 
-    def validate(self, data):
-        user = self.context['request'].user
-        product = data.get('product')
-        #vamowmebt aris tuara produqti ukve damatebuli favoritebshi aam momxmareblistvis
-        if FavoriteProduct.objects.filter(user=user, product=product).exists():
-            raise serializers.ValidationError("This product is already in your favorites.")
-        return data
-
-    def create(self, validated_data):
-        user = self.context['request'].user
-        favorite_product = FavoriteProduct.objects.create(user=user, **validated_data)
-        return favorite_product
+ 
--- products/views.py
+++ products/views.py
@@ -1,146 +1,257 @@
-from django.shortcuts import render
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from products.models import *
-from rest_framework import status
-from products.serializers import *
-from django.shortcuts import get_object_or_404
-from rest_framework.views import APIView
+from django.shortcuts import render 
+from rest_framework.decorators import api_view 
+from rest_framework.response import Response 
+from products.models import * 
+from rest_framework import status 
+from products.serializers import * 
+from django.shortcuts import get_object_or_404 
+from rest_framework.views import APIView 
 
-class ProductListCreateView(APIView):
-    def get(self,request):
-        products = Product.objects.all()
-        serializer = ProductSerializer(products, many=True)
-        return Response(serializer.data)
-    
-    def post(self,request):
-        serializer = ProductSerializer(data=request.data)
-        if serializer.is_valid():
-            product = serializer.save()
-            return Response({"id": product.id}, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class ProductListCreateView(APIView): 
 
-class ProductDetailView(APIView):
-    def get(self,request,pk):
-        obj = get_object_or_404(Product, pk=pk)
-        serializer = ProductSerializer(obj)
-        return Response(serializer.data)
-    
-    def put(self,request,pk):
-        obj = get_object_or_404(Product, pk=pk)
-        serializer = ProductSerializer(obj, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    def path(self,request,pk):
-        obj = get_object_or_404(Product, pk=pk)
-        serializer = ProductSerializer(obj, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def get(self,request): 
+
+        products = Product.objects.all() 
+
+        serializer = ProductSerializer(products, many=True) 
+
+        return Response(serializer.data) 
+    def post(self,request): 
+
+        serializer = ProductSerializer(data=request.data) 
+
+        if serializer.is_valid(): 
+
+            product = serializer.save() 
+
+            return Response({"id": product.id}, status=status.HTTP_201_CREATED) 
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
+
  
-    def delete(self,request,pk):
-        obj = get_object_or_404(Product, pk=pk)
-        obj.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-@api_view(["GET"])
-def product_view(request, pk):
-    obj = get_object_or_404(Product, pk=pk)
-    serializer = ProductSerializer(obj)
-    return Response(serializer.data)
-@api_view(['GET', 'POST'])
-def review_view(request):
-    if request.method == 'GET':
-        reviews = Review.objects.all()
-        review_list = []
-        
-        for review in reviews:
-            review_data = {
-                'id': review.id,
-                'product_id': review.product.id,
-                'content': review.content,
-                'rating': review.rating
-            }
-            review_list.append(review_data)
-        
-        return Response({'reviews': review_list})
-    elif request.method == 'POST':
-        serializer = ReviewSerializer(data=request.data, context={"request":request})
-        if serializer.is_valid():
-            review = serializer.save()
-            return Response(
-                {'id': review.id, 'message': 'Review created successfully!'},
-                status=status.HTTP_201_CREATED
-            )
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-  
-@api_view(['GET', 'POST'])
-def cart_view(request):
-    if request.method == 'GET':
-        cart_items = Cart.objects.all()
-        if not cart_items.exists(): 
-            return Response({'cart': []})  
-        
-        serializer = CartSerializer(cart_items, many=True)
-        return Response({'cart': serializer.data})
-    
-    elif request.method == 'POST':
-        serializer = CartSerializer(data=request.data)
-        if serializer.is_valid():
-            product_id = serializer.validated_data['product_id']
-            quantity = serializer.validated_data['quantity']
-            
-            product = Product.objects.get(id=product_id)
-            cart_item, created = Cart.objects.get_or_create(product=product, defaults={'quantity': quantity})
-            if not created:
-                cart_item.quantity += quantity
-                cart_item.save()
-            
-            return Response({'message': 'Product added to cart'}, status=status.HTTP_201_CREATED)
-        
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-@api_view(['GET', 'POST'])
-def product_tag_view(request, product_id):
-    product = get_object_or_404(Product, id=product_id)
-    
-    if request.method == 'GET':
-        tags = ProductTag.objects.filter(product=product)
-        serializer = ProductTagSerializer(tags, many=True)
+class ProductDetailView(APIView): 
+
+    def get(self,request,pk): 
+
+        obj = get_object_or_404(Product, pk=pk) 
+
+        serializer = ProductSerializer(obj) 
+
+        return Response(serializer.data) 
+    def put(self,request,pk): 
+
+        obj = get_object_or_404(Product, pk=pk) 
+
+        serializer = ProductSerializer(obj, data=request.data) 
+
+        if serializer.is_valid(): 
+
+            serializer.save() 
+
+            return Response(serializer.data) 
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
+    def path(self,request,pk): 
+
+        obj = get_object_or_404(Product, pk=pk) 
+
+        serializer = ProductSerializer(obj, data=request.data, partial=True) 
+
+        if serializer.is_valid(): 
+
+            serializer.save() 
+
+            return Response(serializer.data) 
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
+
+    def delete(self,request,pk): 
+
+        obj = get_object_or_404(Product, pk=pk) 
+
+        obj.delete() 
+
+        return Response(status=status.HTTP_204_NO_CONTENT) 
+
+@api_view(["GET"]) 
+
+def product_view(request, pk): 
+
+    obj = get_object_or_404(Product, pk=pk) 
+
+    serializer = ProductSerializer(obj) 
+
+    return Response(serializer.data) 
+
+@api_view(['GET', 'POST']) 
+
+def review_view(request): 
+
+    if request.method == 'GET': 
+
+        reviews = Review.objects.all() 
+
+        review_list = [] 
+
+        for review in reviews: 
+
+            review_data = { 
+
+                'id': review.id, 
+
+                'product_id': review.product.id, 
+
+                'content': review.content, 
+
+                'rating': review.rating 
+
+            } 
+
+            review_list.append(review_data) 
+
+         
+
+        return Response({'reviews': review_list}) 
+
+    elif request.method == 'POST': 
+
+        serializer = ReviewSerializer(data=request.data, context={"request":request}) 
+
+        if serializer.is_valid(): 
+
+            review = serializer.save() 
+
+            return Response( 
+
+                {'id': review.id, 'message': 'Review created successfully!'}, 
+
+                status=status.HTTP_201_CREATED 
+
+            ) 
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
+
+ 
+
+   
+
+@api_view(['GET', 'POST']) 
+
+def cart_view(request): 
+
+    if request.method == 'GET': 
+
+        cart_items = Cart.objects.all() 
+
+        if not cart_items.exists():  
+
+            return Response({'cart': []})   
+        serializer = CartSerializer(cart_items, many=True) 
+
+        return Response({'cart': serializer.data}) 
+
+    elif request.method == 'POST': 
+
+        serializer = CartSerializer(data=request.data) 
+
+        if serializer.is_valid(): 
+
+            product_id = serializer.validated_data['product_id'] 
+
+            quantity = serializer.validated_data['quantity'] 
+            product = Product.objects.get(id=product_id) 
+            cart_item, created = Cart.objects.get_or_create(product=product, defaults={'quantity': quantity}) 
+
+            if not created: 
+
+                cart_item.quantity += quantity 
+
+                cart_item.save() 
+
+             
+
+            return Response({'message': 'Product added to cart'}, status=status.HTTP_201_CREATED) 
+
+         
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
+
+     
+
+@api_view(['GET', 'POST']) 
+
+def product_tag_view(request, product_id): 
+
+    product = get_object_or_404(Product, id=product_id) 
+    if request.method == 'GET': 
+
+        tags = ProductTag.objects.filter(product=product) 
+
+        serializer = ProductTagSerializer(tags, many=True) 
+
         return Response({'tags': serializer.data})
-    
-    elif request.method == 'POST':
-        serializer = ProductTagSerializer(data=request.data)
-        if serializer.is_valid():
-            tag_name = serializer.validated_data['tag_name']
-            if ProductTag.objects.filter(product=product, tag_name=tag_name).exists():
-                return Response({'error': 'Tag already exists for this product'}, status=status.HTTP_400_BAD_REQUEST)
-            
-            ProductTag.objects.create(product=product, tag_name=tag_name)
-            return Response({'message': 'Tag added successfully'}, status=status.HTTP_201_CREATED)
-        
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'POST'])
-def favorite_product_view(request, user_id):
-    if request.method == 'GET':
-        favorites = FavoriteProduct.objects.filter(user_id=user_id)
-        serializer = FavoriteProductSerializer(favorites, many=True)
-        return Response({'favorites': serializer.data})
-    
-    elif request.method == 'POST':
-        serializer = FavoriteProductSerializer(data=request.data)
-        if serializer.is_valid():
-            product_id = serializer.validated_data['product_id']
-            product = get_object_or_404(Product, id=product_id)
-            if FavoriteProduct.objects.filter(user_id=user_id, product=product).exists():
-                return Response({'error': 'Product is already in favorites'}, status=status.HTTP_400_BAD_REQUEST)
-            
-            FavoriteProduct.objects.create(user_id=user_id, product=product)
-            return Response({'message': 'Product added to favorites successfully'}, status=status.HTTP_201_CREATED)
-        
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'POST': 
+
+        serializer = ProductTagSerializer(data=request.data) 
+
+        if serializer.is_valid(): 
+
+            tag_name = serializer.validated_data['tag_name'] 
+
+            if ProductTag.objects.filter(product=product, tag_name=tag_name).exists(): 
+
+                return Response({'error': 'Tag already exists for this product'}, status=status.HTTP_400_BAD_REQUEST) 
+
+             
+
+            ProductTag.objects.create(product=product, tag_name=tag_name) 
+
+            return Response({'message': 'Tag added successfully'}, status=status.HTTP_201_CREATED) 
+
+         
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
+
+ 
+
+@api_view(['GET', 'POST']) 
+
+def favorite_product_view(request, user_id): 
+
+    if request.method == 'GET': 
+
+        favorites = FavoriteProduct.objects.filter(user_id=user_id) 
+
+        serializer = FavoriteProductSerializer(favorites, many=True) 
+
+        return Response({'favorites': serializer.data}) 
+
+     
+
+    elif request.method == 'POST': 
+
+        serializer = FavoriteProductSerializer(data=request.data) 
+
+        if serializer.is_valid(): 
+
+            product_id = serializer.validated_data['product_id'] 
+
+            product = get_object_or_404(Product, id=product_id) 
+
+            if FavoriteProduct.objects.filter(user_id=user_id, product=product).exists(): 
+
+                return Response({'error': 'Product is already in favorites'}, status=status.HTTP_400_BAD_REQUEST) 
+
+             
+
+            FavoriteProduct.objects.create(user_id=user_id, product=product) 
+
+            return Response({'message': 'Product added to favorites successfully'}, status=status.HTTP_201_CREATED) 
+
+         
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
+
+ 
