384326d08a278babae7462d28e7c0c2acfd892aa
TornikePkhakadze <pheixa@gmail.com>
2025-05-15 18:57:58 +0400

    ,

--- users/tests.py
+++ users/tests.py
@@ -7,48 +7,128 @@ from users.models import EmailVerificationCode
 
 User=get_user_model()
 
-class RegisterViewTest(TestCase):
+# class RegisterViewTest(TestCase):
+#     def setUp(self):
+#         self.client=APIClient()
+#         self.register_url=reverse('register-list')
+#         self.user_data={
+#             'username': 'testuser123',
+#             'email': 'testuser123@gmail.com',
+#             'password': 'strongpassword123',
+#             'password2': 'strongpassword123',
+#             'first_name':'fakefake',
+#             'last_name':'fakefake',
+#             'phone_number':'995568633770',
+
+#         }
+
+#     def test_user_registration_success(self):
+#         response=self.client.post(self.register_url, self.user_data, format='json')
+#         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+#         self.assertTrue(User.objects.filter(email=self.user_data['email']).exists())
+#         user=User.objects.get(email=self.user_data['email'])
+#         self.assertFalse(user.is_active)
+#         self.assertTrue(EmailVerificationCode.objects.filter(user=user).exists())
+
+#     def test_user_registration_password_mismatch(self):
+
+#         self.user_data['password2'] = 'differentpassword123'
+#         response= self.client.post(self.register_url, self.user_data, format='json')
+#         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+#         self.assertIn('password', response.data)
+#         self.assertFalse(User.objects.filter(email=self.user_data['email']).exists())
+    
+#     def test_user_registration_duplicate_mail(self):
+
+#         User.objects.create_user(
+#             username ="random123",
+#             email=self.user_data['email'],
+#             password="randompassword123",
+#         )
+#         response= self.client.post(self.register_url, self.user_data, format='json')
+#         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+#         self.assertIn('email', response.data)
+
+# from django.utils import timezone
+# from users.models import EmailVerificationCode
+
+# class EmailVerificationTest(TestCase):
+#     def setUp(self):
+#         self.client=APIClient()
+
+#         self.user=User.objects.create_user(
+#             username='test123',
+#             email='test123@gmail.com',
+#             password='password123',
+#             is_active=False
+#         )
+
+#         self.confirm_url=reverse('register-confirm-code')
+
+#         self.verification_code=EmailVerificationCode.objects.create(
+#             user=self.user,
+#             code='123456',
+#             created_at=timezone.now()
+#         )
+
+#     def test_successful_verification(self):
+#         data={
+#             'email':'test123@gmail.com',
+#             'code': '123456'
+#         }  
+
+#         response=self.client.post(self.confirm_url, data, format='json')
+#         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+#         self.user.refresh_from_db()
+#         self.assertTrue(self.user.is_active)
+
+#     def test_invalid_code(self):
+#         data={
+#              'email':'test123@gmail.com',
+#              'code':'wrong123'
+#         }
+#         response=self.client.post(self.confirm_url, data, format='json')
+#         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+#         self.user.refresh_from_db()
+#         self.assertFalse(self.user.is_active)
+
+#     def test_expired_code(self):
+#         self.verification_code.created_at=timezone.now()-timezone.timedelta(hours=24)
+#         self.verification_code.save()
+
+#         data={
+#              'email':'test123@gmail.com',
+#              'code': '123456'
+#         }
+#         response=self.client.post(self.confirm_url, data, format='json')
+#         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+#         self.user.refresh_from_db()
+#         self.assertFalse(self.user.is_active)
+
+
+class AuthenticationTest(TestCase):
     def setUp(self):
         self.client=APIClient()
-        self.register_url=reverse('register-list')
-        self.user_data={
-            'username': 'testuser123',
-            'email': 'testuser123@gmail.com',
-            'password': 'strongpassword123',
-            'password2': 'strongpassword123',
-            'first_name':'fakefake',
-            'last_name':'fakefake',
-            'phone_number':'995568633770',
+        self.login_url=reverse('token_obtain_pair')
+        self.user_detail_url=reverse('category-list')
 
-        }
-
-    def test_user_registration_success(self):
-        response=self.client.post(self.register_url, self.user_data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertTrue(User.objects.filter(email=self.user_data['email']).exists())
-        user=User.objects.get(email=self.user_data['email'])
-        self.assertFalse(user.is_active)
-        self.assertTrue(EmailVerificationCode.objects.filter(user=user).exists())
-
-    def test_user_registration_password_mismatch(self):
-
-        self.user_data['password2'] = 'differentpassword123'
-
-        response= self.client.post(self.register_url, self.user_data, format='json')
-
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-
-        self.assertin('password', response.data)
-
-        self.assertFalse(User.objects.filter(email=self.user_data['email']).exists())
-    
-    def test_user_registration_duplicate_mail(self):
-
-        User.objects.create(
-            username ="random123",
-            email=self.user_data['email'],
-            password="randompassword123",
+        self.user=User.objects.create_user(
+            username='test123',
+            email='test123@gmail.com',
+            password='password123',
         )
-        response= self.client.post(self.register_url, self.user_data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertin('email', response.data)
+    
+    def test_unsucessful_token(self):
+        data={
+            'email':'test1234@gmail.com',
+            'password':'password12344',
+        }
+        
+        response=self.client.post(self.login_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.user.refresh_from_db()
+        self.assertFalse(self.user_detail_url)
+
+
+
